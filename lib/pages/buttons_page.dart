58b304d1f89274af9d27261b88ba4d978797ec34
azx <xoscuro3@gmail.com>
2021-05-26 17:30:09 -0400

    paginas de botones

---
+++ lib/pages/buttons_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Buttons extends StatelessWidget {
+  static const String routeName = "/buttons_page";
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: new Container(
+            child: new Center(
+      child: new Text("Buttones"),
+    )));
+  }
+}
