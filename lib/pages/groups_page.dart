e1abcf15282a2d929b3b35999916a8bec46e238e
azx <xoscuro3@gmail.com>
2021-05-26 17:30:58 -0400

    paginas de los grupos

---
+++ lib/pages/groups_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Groups extends StatelessWidget {
+  static const String routeName = "/groups_page";
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: new Container(
+            child: new Center(
+      child: new Text("Groupos"),
+    )));
+  }
+}
